7f65fedd50159f900a8a4b7fe19c0107a3f6989e
Malon <ma12917>
2017-11-28 16:46:32 +0000

    finished documenting square.h

--- square.h
+++ square.h
@@ -10,38 +10,34 @@ class ChessBoard;
 
 
 /**
- * Square. This class represents a square
- * on a chessboard as:
+ * Square. This class represents a square on a chessboard as:
  *   - a pointer to the chessboard it belongs to
- *   - file int in range [0,7]:
- *     file 'a' corresponds to 0, 'b' to 1 etc.
- *   - rank int in range [0,7]:
- *     rank 1 corresponds to 1, 1 to 2 etc.
- *   - if square is empty, piece_ptr points to a Piece
- *     otherwise, points to NULL
+ *   - a file index in range [0, 7] s.t file 0 maps to 'a' & 7 to 'h''
+ *   - a rank index in range [0 ,7] s.t rank 0 maps to 1  & 7 to 8
+ *   - a pointer to the Piece currently on this square or to NULL
+ *     if this square does not have a piece
  */
 class Square{
 
 private:
-  ChessBoard* chessboard;
-  const int file;
-  const int rank;
-  Piece* piece_ptr;
+  ChessBoard* chessboard_p;
+  const int file_i;
+  const int rank_i;
+  Piece* piece_p;
 
 public:
-  // keeps track of number of squares. used for debugging
+  // keeps track of number of squares. used for testing & debugging
   static int num_squares;
 
   
   /**
-   * Constructor which:
-   *    - initializes chessboard_ptr
-   *    - initializes constants file and rank
-   *    - sets piece_ptr to NULL.
-   *    - increments num_squares
+   * Constructor. Initializes chessboard_p to point to the chessboard this
+   * Square belongs to. Initializes file_i & rank_i. sets piece_p to NULL.
+   * Increments num_squares.
    */
-  Square(ChessBoard* chessboard, int file, int rank);
+  Square(ChessBoard* chessboard, int file_i, int rank_i);
 
+  
   /**
    * Destructor which:
    *   - deletes piece_ptr if this square has a piece
@@ -51,102 +47,112 @@ public:
 
   
   /**
-   * Internal helper method which, given a valid destination Square pointer:
-   *   - returns the difference between the destination square's file
-   *     and this square's file
+   * Observer. Returns the difference between the file indices of the 
+   * Square at the given pointer and this Square.
    */
-  const int filesTo(Square* sqr_dest_ptr) const;
+  const int filesTo(Square* dest_p) const;
 
-  /**
-   * Internal helper method which, given a valid destinatin Square pointer:
-   *   - returns the difference between the destination square's rank
-   *     and this square's rank
-   */
-  const int ranksTo(Square* sqr_dest_ptr) const;
-
-  /**
-   * Observer method which returns rank
-   */
-  int getRank(){return rank;}
-
-  /**
-   * Observer method which returns file
-   */
-  int getFile(){return file;}
   
   /**
-   * Method which given a pointer to a destination Square and an array
-   * of 8 NULL Square pointers (longest path <= 8)
-   *   - returns false if there is no horizontal, vertical or diagonal
-   *     path from this square to the destination square
-   *   - otherwise, copies pointers to all squares in that path to the 
-   *     path array, starting with this square and ending with the 
-   *     destination square, then returns true
+   * Observer. Returns the difference between the index indices of the 
+   * Square at the given pointer and this Square.
    */
-  bool getPath(Square* sqr_dest_ptr, Square** path, PieceType piece_type);
+  const int ranksTo(Square* sqr_dest_p) const;
+
+  
+  /**
+   * Observer. Returns this Square's rank index
+   */
+  int getRankIndex(){return rank_i;}
 
 
   /**
-   * Mutator. Requires row array size >= 8. Copies pointers to Squares 
-   * in this Square's row into row, in increasing file index order.
+   * Observer. Returns this Square's file index.
    */
-  void getRow(Square** row);
+  int getFileIndex(){return file_i;}
+
+  
+  /**
+   * Mutator. Requires path array size >= 8. Copies all squares on the 
+   * diagonal, horizontal or vertical path starting at this Square 
+   * & ending with the Square at the given pointer, into the given
+   * path array, then returns true. Returns false if there is no such path.
+   */
+  bool getPath(Square* dest_p, Square** path, PieceType type);
+
 
   /**
-   * Mutator. Requires column array size >= 8. Copies pointers to Squares
-   * in this Square's column into column, in increasing file index order.
+   * Mutator. Requires rank array size >= 8. Copies pointers to Squares 
+   * in this Square's rank into rank, in increasing file index order.
    */
-  void getColumn(Square** column);
+  void getRank(Square** rank);
+
+  /**
+   * Mutator. Requires file array size >= 8. Copies pointers to Squares
+   * in this Square's file into file, in increasing rank index order.
+   */
+  void getFile(Square** file);
 
 
   /**
    * Mutator. Requires diagonal array size >= 8. Copies pointers to Squares
    * in the chessboard diagonal that contains both this square and the one
-   * sqr_dest_ptr points to, in increasing file order.
+   * at the given pointer, in increasing file order.
    */
-  void getDiagonal(Square* sqr_dest_ptr, Square** diagonal);
+  void getDiagonal(Square* dest_p, Square** diagonal);
 
-  // PIECE RELATED METHODS //
   
   /**
-   *  Method which returns true if this square has no piece
+   *  Observer. Returns true if this square has a piece.
    */
-  bool isEmpty() const;
+  bool hasPiece() const;
 
 
   /**
-   * Reader method which returns a pointer to this square's piece
+   * Observer. Returns a pointer to this square's piece.
    */
-  Piece* getPiece() const {return piece_ptr;}
+  Piece* getPiece() const {return piece_p;}
+
   
-  /** 
-   * Mutator method which given a piece pointer:
-   *  - deletes this square's current piece if it has one
-   *  - sets its piece pointer to the given piece pointer
+  /**
+   * Mutator. calls destroyPiece method then sets its piece pointer 
+   * equal to the given piece pointer.
    */ 
-  void setPiece(Piece* piece_ptr);
+  void setPiece(Piece* piece_p);
 
+  
   /**
-   * Mutator method which delete this square's piece if it has one
-   * and sets its pointer to NULL
+   * Mutator. Frees memory allocated to this square's piece if it has one
+   * then sets its pointer to NULL.
    */
   void destroyPiece();
+  
 
   /**
-   * Mutator method which given a pointer to a valid destination square
-   *   - if this square has a piece, which can legally move to the
-   *     the destination square:
-   *      > delete the destination square's piece if it has one
-   *      > moves this square's piece to the destination square
-   *      > sets this square's piece pointer to NULL
-   *      > updates the moved piece to indicate it has moved
+   * Mutator. Requires this Square has a piece, which is of the color that
+   * is play next. If moving the piece from this square to the square at
+   * given pointer is legal as per chess rules, this method calls 
+   * the destination Square's destroy method on it,  moves this 
+   * square's piece to it, updates the moved piece to indicate it has moved, 
+   * then sets this square's piece pointer to NULL. Returns true if piece 
+   * what moved. Returns false otherwise.
    */
-  bool movePiece(Square* sqr_dest_ptr);
-
-  
-  bool movePutsKingInCheck(Square* sqr_dest_ptr);
+  bool movePiece(Square* dest_p);
 
 
+  /**
+   * Mutator. Requires this Square to have a piece. Returns true if moving
+   * this Square's piece to the Square at the given pointer would put the 
+   * king of the player in check, as per chess rules. This method will undo
+   *  any mutation it induces.
+   */
+  Bool movePutsKingInCheck(Square* dest_p);
+
+
+  /**
+   * Mutator. Requires this Square to have a piece. Returns true if this 
+   * Square's piece can move to a square.
+   */
   bool pieceCanMove();
 };
 
